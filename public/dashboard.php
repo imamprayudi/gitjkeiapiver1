8a2255353db1be952af86e9cec076b8ef5811bdd
imamprayudi <imamprayudi@gmail.com>
2025-10-13 14:15:47 +0700

    favicon

--- public/dashboard.php
+++ public/dashboard.php
@@ -10,6 +10,7 @@ if (isset($_SESSION['user'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <title>Dashboard</title>
+  <link id="favicon" rel="icon" type="image/png" href="assets/gambar/g-green.png">
 </head>
 <body>
 <?php include 'menu.php'; ?>
